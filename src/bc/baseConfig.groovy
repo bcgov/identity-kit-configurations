88a2ff43481fa8709e6d571da49e4d908fbb93d4
Emiliano Suñé <emiliano.sune@gmail.com>
2021-03-05 17:19:01 -0800

    Initial pipeline setup
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- src/bc/baseConfig.groovy
+++ src/bc/baseConfig.groovy
@@ -1,4 +1,4 @@
-
+    
 package bc;
 
 class baseConfig {
@@ -6,11 +6,17 @@ class baseConfig {
   public static final int WAIT_TIMEOUT = 10
 
   // Deployment Environment TAGs
-  public static final String[] DEPLOYMENT_ENVIRONMENT_TAGS = ['test', 'prod']
+  public static final String[] DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
 
   // The name of the project namespace(s).
-  public static final String  NAME_SPACE = 'devex-von'
+  public static final String  NAME_SPACE = 'a99fd4'
 
-  // Apps - Listed in the order they should be tagged
-  public static final String[] APPS = ['issuer-agent', 'issuer-api', 'issuer-db', 'issuer-wallet', 'issuer-web', 'issuer-admin']
+  // Instance Suffix
+  public static final String  SUFFIX = ''
+
+  // Apps - Listed in the order they should be built
+  public static final String[] APPS = ['issuer-kit-db', 'issuer-kit-wallet', 'issuer-kit-admin', 'issuer-kit-web', 'issuer-kit-agent', 'issuer-kit-api']
+
+  // Apps - Listed in the order they should be tagged for deployment
+  public static final String[] DEPLOY_APPS = ['issuer-kit-db', 'issuer-kit-wallet', 'issuer-kit-admin', 'issuer-kit-web', 'issuer-kit-agent', 'issuer-kit-api']
 }
