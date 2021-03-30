0718e9aa88cac11ad58a2abaaea47ef887cebe96
wadeking98 <wkingnumber2@gmail.com>
2021-03-30 13:13:18 -0700

    dev deployment working
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- openshift/templates/issuer-web/issuer-web-deploy.overrides.sh
+++ openshift/templates/issuer-web/issuer-web-deploy.overrides.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 _includeFile=$(type -p overrides.inc)
 if [ ! -z ${_includeFile} ]; then
   . ${_includeFile}
@@ -24,9 +25,9 @@ generateConfigMap "${CADDY_CONFIG_MAP_NAME}${SUFFIX}" "${CADDY_SOURCE_FILE}" "${
 # - DEPLOYMENT_ENV_NAME
 
 # Combine the profile's default config files with its environment specific config files before generating the config map ...
-profileRoot=$( dirname "$0" )/config/${PROFILE}
+profileRoot=$( dirname "$0" )/config
 profileEnv=${profileRoot}/${DEPLOYMENT_ENV_NAME}
-profileTmp=$( dirname "$0" )/config/${PROFILE}/tmp
+profileTmp=$( dirname "$0" )/config/tmp
 mkdir -p ${profileTmp}
 cp -f ${profileRoot}/* ${profileTmp} 2>/dev/null
 cp -f ${profileEnv}/* ${profileTmp} 2>/dev/null
